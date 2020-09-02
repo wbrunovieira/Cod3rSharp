06a649ef01b1b8b908ba55f2992d8f825206e906
wbrunovieira <wbrunovieira77@gmail.com>
2020-09-02 09:42:52 +0100

    organizations

---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CursoCSharp.Fundamentos
+{
+    public class PrimeiroPrograma
+    {
+        public PrimeiroPrograma()
+        {
+            static void Main()
+            {
+                Console.Write("Primeiro");
+                Console.WriteLine("Programa");
+                Console.WriteLine("Terminou");
+            }
+        }
+    }
+}
